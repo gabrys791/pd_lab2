421944b50a4e27d55c75453cea7a29793a111311
gabrys791 <100271821+gabrys791@users.noreply.github.com>
2022-03-06 17:16:12 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+#zad 1
+# lista_sportów=['siatkówka','koszykówka','tenis-stołowy']
+# lista_sportów.reverse()
+# lista_sportów.append('piłka nożna')
+# lista_sportów.append('boks')
+# print(lista_sportów)
+#zad 2
+# słownik={'cdn':'ciąg dalszy nastąpi','CV':'curriculum vitae','lic':'licencjat','mgr':'magister','dr':'doktor'}
+# print(słownik)
+#zad 3
+słownik={1:'League of legends',2:'Valorant',3:'Kena Bridge of Spirits',4:'Apex legends'}
+print(len(słownik))
+#zad 4
